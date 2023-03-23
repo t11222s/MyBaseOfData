7d9e07d0aa6cf9fdc174f2182eeaaf74418695b8
t11222s <77324305+t11222s@users.noreply.github.com>
2023-03-23 21:20:16 +0530

    Add files via upload

---
+++ Models/post.cs
@@ -0,0 +1,15 @@
+﻿namespace w2y.Models
+{
+    public class post
+    {
+        public int postId { get; set; }
+        public string postTitle { get; set; }
+
+        public string content{ get; set; }
+
+        public post()
+        {
+
+        }
+    }
+}
